337687f5c4e332acbb7916c2462471c286b3f405
Максим <nadiamelnik263@gmail.com>
2026-07-03 08:41:11 +0300

    add password change/reset

--- django_project/settings.py
+++ django_project/settings.py
@@ -133,5 +133,7 @@ AUTH_USER_MODEL = "accounts.CustomUser"
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
 
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"  # new
-CRISPY_TEMPLATE_PACK = "bootstrap5"  # new
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"  
+CRISPY_TEMPLATE_PACK = "bootstrap5"  
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
